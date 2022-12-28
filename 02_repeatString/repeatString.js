c0987859dc74e5c345944fa00b4ef94176c5bf4e
Nathan <nthn.wei@gmail.com>
2022-12-28 17:50:13 +0000

    Complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+	if (times < 0) return "Please specify a number of times to repeat.";
+	let output = '';
+	for (let i = 0; i < times; i++) {
+		output += str;
+	}
+	return output;
 };
 
 // Do not edit below this line
