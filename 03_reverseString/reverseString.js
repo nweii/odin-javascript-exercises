c83364323dc1cb483ea4ecd6435cfe31921e3a81
Nathan <nthn.wei@gmail.com>
2022-12-28 22:57:07 +0000

    Create reverse string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+	let output = ''; // initially blank so we can add characters
+	
+	// iterate on each character, starting with the last
+	for (let i = str.length - 1; i >= 0; i--) {
+		output += str.charAt(i);
+	}
+	
+	return output;
 };
 
 // Do not edit below this line
