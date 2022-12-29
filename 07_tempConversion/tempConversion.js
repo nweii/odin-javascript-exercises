f9839472be31c28d79c22c9d32188020b6767ea8
Nathan <nthn.wei@gmail.com>
2022-12-29 05:06:21 +0000

    Complete exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fht) {
+  return Number(((5/9) * (fht - 32)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  return Number(((cel * 1.8) + 32).toFixed(1));
 };
 
 // Do not edit below this line
