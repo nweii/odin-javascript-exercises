ea9f754fb6a84ce6b867673f9beef961156133ae
Nathan <nthn.wei@gmail.com>
2022-12-29 00:11:29 +0000

    Complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+	if (!Number.isInteger(n1) || !Number.isInteger(n2)) {
+		return 'ERROR';
+	}
+	if (n1 < 0 || n2 < 0) {
+		return 'ERROR';
+	}
+	if (n1 > n2) {
+		const max = n1; // set aside n1
+		n1 = n2; // overwrite n1 with n2
+		n2 = max; // overwrite n2 with what used to be n1
+	}
+	
+	let sum = 0;
+	for (let i = n1; i <= n2; i++) {
+		sum += i;
+	}
+	
+	return sum;
 };
 
 // Do not edit below this line
