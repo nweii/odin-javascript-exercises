b4173ad8ec5d2806e37881468c47277eba650892
Nathan <nthn.wei@gmail.com>
2023-01-30 18:02:28 +0000

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+	return people.reduce((oldest, currentPerson) => {
+		const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+		const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+		return oldestAge < currentAge ? currentPerson : oldest;
+	})
+};
 
+const getAge = function(birth, death) {
+	if (!death) {
+		// base age calc on present year instead of a death year
+		death = new Date().getFullYear();
+	}
+	return death - birth;
 };
 
 // Do not edit below this line
