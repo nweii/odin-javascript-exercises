da73ce018188a82619424d0e082d670fdca8cbad
Nathan <nthn.wei@gmail.com>
2023-01-30 16:31:52 +0000

    Complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+	if (n === 0 || n < 0) return "OOPS";
+	
+	// the first 'preceding' two numbers
+	let a = 0;
+	let b = 1;
+	
+	for (let i = 1; i < n; i++) {
+		const temp = b;
+		b = a + b; // add prev two nums to get this fibonacci
+		a = temp; // set up new preceding number
+	}
+	return b;
 };
 
 // Do not edit below this line
