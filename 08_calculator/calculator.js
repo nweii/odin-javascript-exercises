0ea4ab1964700e59acaf3c0a57006f59f15f1ca6
Nathan <nthn.wei@gmail.com>
2023-01-30 15:45:01 +0000

    Complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  // start 'total' at 0 and add each 'current' number in the array to the running total
+	return nums.reduce((total, current) => total + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, current) => total * current)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) return 1;
+  return n * factorial(n-1);
 };
 
 // Do not edit below this line
